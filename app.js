e1ab887168f3766debde70bc1431271fc6daa195
Jaypeexx <pauloalcala26@gmail.com>
2025-02-06 17:22:47 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Este archivo está listo para agregar interactividad si se desea
+// Puedes agregar más funciones a tu página web en el futuro
+
+console.log("Página Words cargada correctamente.");
